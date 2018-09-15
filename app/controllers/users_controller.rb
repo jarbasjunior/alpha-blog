e23e05f84e4dbb75ae97f5146a361748d121e487
jarbasjunior <jarbasoujunior@gmail.com>
2018-09-15 10:32:45 -0300

    Add edit and update funcionalitys partial

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
 
+  def index
+    @users = User.all
+  end
+
 	def new
 		@user = User.new
 	end
@@ -14,10 +18,32 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "Usuário atualizado com sucesso"
+      redirect_to articles_path(@user)
+    else
+      render 'edit'
+    end  
+  end
+
+   def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    flash[:success] = "Uusário removido com sucesso"
+    redirect_to users_path
+  end
+
   private
-    def set_user
-      @user = user.find(params[:id])
-    end
     def user_params
       params.require(:user).permit(:username, :email, :password)  
     end
